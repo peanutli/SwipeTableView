71092f5a896891ad8c191045e38c5dee7d2bc7be
花生 <bjlidachuan@163.com>
2016-06-04 21:39:35 +0800

    添加这个疑问怎么使用storyboard

--- SwipeTableView/SwipeTableViewDemo/DemoListController.m
+++ SwipeTableView/SwipeTableViewDemo/DemoListController.m
@@ -52,6 +52,7 @@
 }
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
+//这个是怎么传的viewController
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
